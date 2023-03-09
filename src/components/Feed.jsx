4bcab33c5306417d25ed632f1b90b577adfa8fe1
jonrusso <russovfx@icloud.com>
2023-03-08 22:17:36 -0300

    Update Feed.jsx
    new features for the tab comments using useState

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,20 +3,30 @@ import { format, formatDistanceToNow } from 'date-fns'
 import styles from './Feed.module.css'
 import { Comment } from './Comment.jsx'
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
-const comments = [
-  1,
-  2,
-  3,
-];
+
 
 export function Feed({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ])
+
   const publishedDateFormatted = format(publishedAt, "d LLLL 'at' hh:mm a")
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     addSuffix: true,
 
   })
+
+  function handleCreateNewComment(){
+    event.preventDefault()
+
+    setComments([...comments, comments.length + 1]) ;
+
+    console.log('oi');
+  }
 
   return (
     <article className={styles.feed}>
@@ -41,7 +51,7 @@ export function Feed({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Leave your feedback</strong>
         <textarea
           placeholder="Leave your comments here"
