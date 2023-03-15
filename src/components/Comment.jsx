4b9046ebbd8c58f4c1a6e45b0c103809e12b50bf
jonrusso <russovfx@icloud.com>
2023-03-15 20:31:22 -0300

    feat: likeCount ⚙️

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,19 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
   }
 
   return (
@@ -25,9 +34,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Likes <span>20</span>
+            Likes <span>{likeCount}</span>
           </button>
         </footer>
       </div>
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -26,12 +26,25 @@ export function Feed({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
   }
 
-  function deleteComment(comment) {
-    console.log(`Deletar comentário ${comment}`);
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity(
+      "Thanks for trying post without filling out"
+    );
   }
+
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
+      return comment !== commentToDelete;
+    });
+
+    setComments(commentsWithoutDeletedOne);
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.feed}>
@@ -72,9 +85,13 @@ export function Feed({ author, publishedAt, content }) {
           name="comment" //we can also use input as a name property
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
         <footer>
-          <button type="submit">Add Comment</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Add Comment
+          </button>
         </footer>
       </form>
 
